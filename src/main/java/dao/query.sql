31023f05fa6b69845663bf1415354f6213c7c8a8
SCY <helenason@naver.com>
2024-04-01 00:33:28 +0900

    fix(sql): 테이블 생성 쿼리문 수정

--- src/main/java/dao/query.sql
+++ src/main/java/dao/query.sql
@@ -1,15 +1,16 @@
- CREATE TABLE board (
- 	board_id INT NOT NULL auto_increment,
+CREATE TABLE game (
+    game_id INT NOT NULL auto_increment,
+    turn VARCHAR(10) NOT NULL,
+    PRIMARY KEY (game_id)
+);
+
+CREATE TABLE board (
+	board_id INT NOT NULL auto_increment,
     file_column INT NOT NULL,
     rank_row INT NOT NULL,
     piece_type VARCHAR(10) NOT NULL,
     piece_color VARCHAR(10) NOT NULL,
- 	PRIMARY KEY (board_id)
- );
-
- CREATE TABLE game (
- 	game_id INT NOT NULL auto_increment,
-    game VARCHAR(50) NOT NULL,
-    turn VARCHAR(10) NOT NULL,
-    PRIMARY KEY (id)
- )
+    game_id INT NOT NULL,
+	PRIMARY KEY (board_id),
+	FOREIGN KEY (game_id) references game(game_id)
+);
