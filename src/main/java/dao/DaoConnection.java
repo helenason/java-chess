b58385bc0d145ef593056181566bb46e99841cbb
SCY <helenason@naver.com>
2024-04-05 16:01:20 +0900

    refactor(DaoConnection): 데이터베이스 연결 실패 시 에러 반환

--- src/main/java/dao/DaoConnection.java
+++ src/main/java/dao/DaoConnection.java
@@ -12,11 +12,15 @@ public class DaoConnection {
     private static final String USERNAME = "username";
     private static final String PASSWORD = "password";
 
-    public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(
-                String.format("jdbc:mysql://%s/%s%s", SERVER, DATABASE, OPTION),
-                USERNAME,
-                PASSWORD
-        );
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                    String.format("jdbc:mysql://%s/%s%s", SERVER, DATABASE, OPTION),
+                    USERNAME,
+                    PASSWORD
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("[DB_ERROR] 데이터베이스 에러입니다. 관리자에게 문의해주세요.");
+        }
     }
 }
