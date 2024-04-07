f90da033c2f036a09a09f6513f947d91197b4b13
SCY <helenason@naver.com>
2024-04-07 23:27:43 +0900

    test(FakeGameDao): fake 저장공간 형식 수정

--- src/test/java/dao/fake/FakeGameDao.java
+++ src/test/java/dao/fake/FakeGameDao.java
@@ -4,48 +4,48 @@ import dao.GameDao;
 import domain.board.Turn;
 import dto.GameData;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 
 public class FakeGameDao implements GameDao {
 
-    private final List<GameData> games = new ArrayList<>();
+    private final Map<Integer, Turn> games = new HashMap<>();
 
     @Override
     public int save(Turn turn) {
         int id = games.size() + 1;
-        games.add(new GameData(id, turn));
+        games.put(id, turn);
         return id;
     }
 
     @Override
     public List<GameData> findAll() {
-        return Collections.unmodifiableList(games);
+        List<GameData> result = new ArrayList<>();
+        for (Entry<Integer, Turn> entry : games.entrySet()) {
+            int id = entry.getKey();
+            Turn turn = entry.getValue();
+            result.add(new GameData(id, turn));
+        }
+        return result;
     }
 
     @Override
     public Optional<Turn> findTurnById(int id) {
-        return games.stream()
-                .filter(gameData -> gameData.id() == id)
-                .findFirst()
-                .map(GameData::turn);
+        return Optional.ofNullable(games.get(id));
     }
 
     @Override
     public int updateById(int id, Turn turn) {
-        deleteById(id);
-        games.add(new GameData(id, turn));
+        games.replace(id, turn);
         return 1;
     }
 
     @Override
     public int deleteById(int id) {
-        GameData target = games.stream()
-                .filter(gameData -> gameData.id() == id)
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR]"));
-        games.remove(target);
+        games.remove(id);
         return 1;
     }
 }
